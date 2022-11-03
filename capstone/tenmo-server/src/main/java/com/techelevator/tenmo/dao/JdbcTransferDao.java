6af474fdd07e9df366ac2b165ab78adc1b38d553
Martell Bowers <martellbowers@hotmail.com>
2022-11-03 10:57:30 -0400

    moving ever so slowly towards goal

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
@@ -1,9 +1,10 @@
 package com.techelevator.tenmo.dao;
 
-import com.techelevator.tenmo.model.Account;
 import com.techelevator.tenmo.model.Transfer;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import java.math.BigDecimal;
 
 public class JdbcTransferDao implements TransferDao{
 
@@ -22,6 +23,11 @@ public class JdbcTransferDao implements TransferDao{
         return null;
     }
 
+    @Override
+    public Transfer resultToTransfer(int transferId) {
+        return null;
+    }
+
     public void createTransfer(Transfer transfer) {
         String sql = "INSERT INTO transfer (transfer_type_id, transfer_status_id, account_from, account_to, amount) " +
                 "VALUES (?, ?, ?, ?, ?, ?)";
@@ -30,23 +36,48 @@ public class JdbcTransferDao implements TransferDao{
 
 
 
-    public Account transferByTransferId(int transferId) {
+    public int transferByTransferId(int transferId) {
         //Step 1 - declare the return type
-        Account account = null;
+
         //Step 2 - create the sql
-        String sql = "SELECT account_id, user_id, balance " +
-                "FROM account " +
-                "WHERE user_id = ?;";
+        String sql = "SELECT transfer_id, transfer_type_id, transfer_status_id, account_from, account_to, amount ?" +
+                "FROM transfer ?" +
+                "WHERE transfer_id = ?;";
         //Step 3 - send sql to database
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, transferId);
         //Step 4 - convert to return type
+        Transfer transfer = null;
 
         // Create a method for mapAccount???
         if (results.next()) {
-            account = mapAccount(results);
+            transfer = mapResultToTransfer(results);
             //Step 5 - return
 
         }
-        return null;
+        return transferId;
+    }
+
+    private Transfer mapResultToTransfer(SqlRowSet results) {
+        Transfer t = new Transfer();
+
+        int transfer_id = results.getInt("transfer_id");
+        t.setTransferId(transfer_id);
+
+        int transfer_type_id = results.getInt("transfer_type_id");
+        t.setTransferTypeId(transfer_type_id);
+
+        int transfer_status_id = results.getInt("transfer_status_id");
+        t.setTransferStatusId(transfer_status_id);
+
+        int account_from = results.getInt("account_from");
+        t.setAccountFrom(account_from);
+
+        int account_to = results.getInt("account_to");
+        t.setAccountTo(account_to);
+
+        BigDecimal amount = results.getBigDecimal("amount");
+        t.setAmount(amount);
+
+        return t;
     }
 }
--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
@@ -1,6 +1,7 @@
 package com.techelevator.tenmo.dao;
 
 import com.techelevator.tenmo.model.Transfer;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 public interface TransferDao {
 
@@ -11,5 +12,9 @@ public interface TransferDao {
     Transfer getTransferByUserId(int transferId);
 
     //public Transfer updateBalances();
+
+    Transfer resultToTransfer (int transferId);
+
+    }
 
 }
